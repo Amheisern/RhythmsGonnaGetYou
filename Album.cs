0bae8da6d83d020d66b438d390bd7dd5a0de6885
lodashreload <icarusjapan@gmail.com>
2022-01-15 13:44:04 -0500

    finished album class

--- Album.cs
+++ Album.cs
@@ -1,7 +1,13 @@
+using System;
+
 namespace RhythmsGonnaGetYou
 {
     public class Album
     {
-
+        public string Title { get; set; }
+        public bool IsExplicit { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int BandId { get; set; }
+        public int Id { get; set; }
     }
 }
