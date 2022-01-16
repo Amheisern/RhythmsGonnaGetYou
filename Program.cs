101da4ae03068f0b536938e80c49d8a748052620
lodashreload <icarusjapan@gmail.com>
2022-01-16 13:41:56 -0500

    need to build out the code to add to database and skeleton done though

--- Program.cs
+++ Program.cs
@@ -53,12 +53,55 @@ namespace RhythmsGonnaGetYou
 
                 Console.WriteLine();
                 Console.WriteLine("Please choose from the menu");
-                Console.WriteLine("(A)dd a new band to database");
+                Console.WriteLine("(A)dd a new data to database");
                 Console.WriteLine("(D)isplay database information");
                 Console.WriteLine("(U)pdate signed status of band");
                 Console.WriteLine("(Q)uit to exit menu");
                 var choice = Console.ReadLine().ToUpper();
 
+                switch (choice)
+                {
+                    case "A":
+                        Console.WriteLine();
+                        Console.WriteLine("1. Please choose from the menu");
+                        Console.WriteLine("2. Add a new band to database");
+                        Console.WriteLine("3. Add a new album to database");
+                        Console.WriteLine("4. Add a new song and to database");
+                        Console.WriteLine("5. Quit to exit menu");
+                        var addChoice = Console.ReadLine().ToUpper();
+                        if (addChoice == "1")
+                        {
+
+                        }
+                        else if (addChoice == "2")
+                        {
+
+                        }
+                        else if (addChoice == "3")
+                        {
+
+                        }
+                        else if (addChoice == "4")
+                        {
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Goodbye!");
+                        }
+
+                        break;
+                    case "D":
+                        break;
+                    case "U":
+                        break;
+                    case "Q":
+                        keepGoing = false;
+                        break;
+                    default:
+                        Console.WriteLine("That was not a valid selection! Please Try again");
+                        break;
+                }
             }
         }
     }
