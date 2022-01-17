a1398387cce19e54722ccc043030431a716a7b87
lodashreload <icarusjapan@gmail.com>
2022-01-17 13:02:23 -0500

    add a property to band

--- Band.cs
+++ Band.cs
@@ -7,6 +7,7 @@ namespace RhythmsGonnaGetYou
         public string Name { get; set; }
         public string ContactName { get; set; }
         public string Genre { get; set; }
+        public string CountryOfOrigin { get; set; }
         public string Website { get; set; }
         public int NumberOfMembers { get; set; }
         public bool IsSigned { get; set; }
